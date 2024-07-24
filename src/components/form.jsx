15ee9ce6c70ce29b1675a08bdd49ffe071d36287
amagolden <amagoldenstein@gmail.com>
2024-07-24 08:00:46 -0400

    added new form

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,6 +1,9 @@
 const Form = () => {
     return (
-    <div>Hello form</div>
+    <form>
+        <input type='text' id='task-id' name='task' onChange={event => console.log(event.target.value)}></input>
+        <input type='submit' value='Add Task'></input>
+    </form>
     );
 }
 
